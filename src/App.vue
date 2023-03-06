d660745dc6f3060edd1b705c5d764fb1a00fc8f1
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-06 13:28:37 +0100

    chiamata con variabili

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   created() {
     axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?&api_key=4f5c0b616a8dd8300db5bb310c4ad301&query=il+signore+degli+anelli`
+        `${store.baseUri}/search/movie?&api_key=${store.apiKey}&query=il+signore+degli+anelli`
       )
       .then((response) => {
         console.log(response.data.results[0].original_title);
