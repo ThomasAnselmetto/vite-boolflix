28036e8737896e50a2e6bfc645ddba858d67f875
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-06 13:27:38 +0100

    chiamata axios con stringa no variabili store.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
 import { store } from "./data/store";
+// divido import da export per chiarezza
 export default {
   data() {
     return {
@@ -10,6 +12,15 @@ export default {
 
   components: {
     AppHeader,
+  },
+  created() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?&api_key=4f5c0b616a8dd8300db5bb310c4ad301&query=il+signore+degli+anelli`
+      )
+      .then((response) => {
+        console.log(response.data.results[0].original_title);
+      });
   },
 };
 </script>
